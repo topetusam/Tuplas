4f20fce6e9375baf8657cbc31e7736de3edcb368
topetusam <67940360+topetusam@users.noreply.github.com>
2024-02-26 09:04:01 -0500

    ejercisiotuplas

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+Nombre=input("Ingrese su Nombre")
+Edad=input("Ingrese su Edad")
+Curso=input("Ingrese si ha echo cursos (S/N)")
+Direccion=input("Ingrese su Direccion")
+Experiencia_laboral=input("Ingrese si tiene experiencia (S/N)")
+
+informacion= tuple((Nombre, Edad, Curso, Direccion, Experiencia_laboral))
+
+print("Los datos del seleccionado es :",informacion)
