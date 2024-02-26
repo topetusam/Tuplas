34ece9ed0318aa32a3911ba8584155c220788642
topetusam <67940360+topetusam@users.noreply.github.com>
2024-02-26 13:37:39 -0500

    ejercisiohechotuplas

--- tuplas.py
+++ tuplas.py
@@ -1,12 +1,12 @@
 Nombre=input("Ingrese su Nombre")
 Edad=input("Ingrese su Edad")
 Direccion=input("Ingrese su Direccion")
-Experiencia_laboral=input("Ingrese si tiene experiencia (S/N)")
-informacion= tuple((Nombre, Edad, Direccion, Experiencia_laboral))
+informacion= tuple((Nombre, Edad, Direccion,))
 
 print("Los datos del seleccionado es :",informacion)
 
-Cursos1=0
+cursorea_lizados=[]
+
 
 Curso=int(input("Ingrese el numero de cursos,si no tiene coloque 0 :"))
 for i in range(Curso):
@@ -14,8 +14,22 @@ for i in range(Curso):
         print("No ha realizado curso")
     else:
         Cursos1=input("Ingrese los cursos que ha realizado")
-        print(Cursos1)
-
-Cursorealizados=tuple((Cursos1))        
-print("los cursos realizados por el aspirante son :", Cursorealizados)
+        cursorea_lizados.append(Cursos1)
         
+
+cursorealizados=tuple((cursorea_lizados))        
+print("los cursos realizados por el aspirante son :", cursorealizados)
+
+Experiencia_Laboral=[]
+
+Experiencia=int(input("En cuantos trabajos ha estado, por favor responda con un numero :"))
+for i in range(Experiencia):
+    if Experiencia==0:
+        print("no tiene experiencia")
+    else:
+        Experiencia1=input("ingrese las empresas en las que ha estado")
+        Experiencia_Laboral.append(Experiencia1)
+
+experienciarealizada=tuple((Experiencia_Laboral))
+print("El aspirante tiene experiencia en las siguientes empresas", experienciarealizada)       
+ 
